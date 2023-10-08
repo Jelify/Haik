e5a9bbfb223ffd21bd34fb0fc43ce28dec948a03
Robert Smith <38408764+Jelify@users.noreply.github.com>
2023-10-08 17:39:48 +0800

    Initial commit

---
+++ Source/Haik/HaikGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HaikGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHaikGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHaikGameMode();
+};
+
+
+
